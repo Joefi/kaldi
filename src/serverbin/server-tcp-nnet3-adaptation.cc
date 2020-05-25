bd41d78846e95ebccb5eab73b84324c8d1e141d9
laine <977579545@qq.com>
2020-05-25 17:32:19 +0800

    fixed error on server-tcp-nnet3-adaptation.cc

--- src/serverbin/server-tcp-nnet3-adaptation.cc
+++ src/serverbin/server-tcp-nnet3-adaptation.cc
@@ -9,42 +9,46 @@
 
 #include "util/kaldi-thread.h"
 #include "nnet3/nnet-utils.h"
+#include "base/kaldi-common.h"
+#include "matrix/matrix-common.h"
 
+namespace kaldi {
 
-class TcpServer {
-public:
-    explicit TcpServer(int read_timeout);
-    ~TcpServer();
+    class TcpServer {
+    public:
+        explicit TcpServer(int read_timeout);
+        ~TcpServer();
 
-    bool Listen(int32 port);  // start listening on a given port
-    int32 Accept();  // accept a client and return its descriptor
+        bool Listen(int32 port);  // start listening on a given port
+        int32 Accept();  // accept a client and return its descriptor
 
-    bool ReadChunk(size_t len); // get more data and return false if end-of-stream
+        bool ReadChunk(size_t len); // get more data and return false if end-of-stream
 
-    Vector<BaseFloat> GetChunk(); // get the data read by above method
+        Vector<BaseFloat> GetChunk(); // get the data read by above method
 
-    int16* GetBuffer();
+        int16* GetBuffer();
 
-    bool Write(const std::string& msg); // write to accepted client
-    bool WriteLn(const std::string& msg, const std::string& eol = "\n"); // write line to accepted client
+        bool Write(const std::string& msg); // write to accepted client
+        bool WriteLn(const std::string& msg, const std::string& eol = "\n"); // write line to accepted client
 
-    void Disconnect();
+        void Disconnect();
 
-private:
-    struct ::sockaddr_in h_addr_;
-    int32 server_desc_, client_desc_;
-    int16* samp_buf_;
-    size_t buf_len_, has_read_;
-    pollfd client_set_[1];
-    int read_timeout_;
-};
+    private:
+        struct ::sockaddr_in h_addr_;
+        int32 server_desc_, client_desc_;
+        int16* samp_buf_;
+        size_t buf_len_, has_read_;
+        pollfd client_set_[1];
+        int read_timeout_;
+    };
+}
 
 int main(int argc, char* argv[]) {
     try
     {
         const char* usage =
             "Reads in audio zip file from a network socket and performs adaptation\n"
-            "with neural nets (nnet3 setup), 
+            "with neural nets (nnet3 setup),\n" 
             "\n"
             "Usage: server-tcp-nnet3-adaptation [options] <nnet3-in> "
             "<fst-in> <word-symbol-table>\n";
