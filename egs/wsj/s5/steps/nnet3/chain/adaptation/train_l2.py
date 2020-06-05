ffc4f69b26e9d7eef994b4369067c6bead7dabcd
laine <977579545@qq.com>
2020-06-05 10:37:52 +0800

    fixed error in train_l2.py

--- egs/wsj/s5/steps/nnet3/chain/adaptation/train_l2.py
+++ egs/wsj/s5/steps/nnet3/chain/adaptation/train_l2.py
@@ -18,10 +18,10 @@ import sys
 import traceback
 
 sys.path.insert(0, 'steps')
-import libs_adapt.nnet3.train.common as common_train_lib
-import libs_adapt.common as common_lib
-import libs_adapt.nnet3.train.chain_objf.acoustic_model as chain_lib
-import libs_adapt.nnet3.report.log_parse as nnet3_log_parse
+import libs.nnet3.adaptation.common as common_train_lib
+import libs.common as common_lib
+import libs.nnet3.adaptation.chain_l2.acoustic_model as chain_lib
+import libs.nnet3.report.log_parse as nnet3_log_parse
 
 
 logger = logging.getLogger('libs')
