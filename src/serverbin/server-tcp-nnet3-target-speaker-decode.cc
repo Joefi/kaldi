6746a2cfd82c435f43c5796570c75628285c071e
laine <977579545@qq.com>
2020-06-15 21:29:08 +0800

    fixed error in target speaker decode

--- src/serverbin/server-tcp-nnet3-target-speaker-decode.cc
+++ src/serverbin/server-tcp-nnet3-target-speaker-decode.cc
@@ -241,7 +241,7 @@ int main(int argc, char* argv[]) {
             eos = !server.ReadChunk(sizeof(recv_speaker_name));
             server.GetBuffer(recv_speaker_name, sizeof(recv_speaker_name));
 
-            std::string speaker_name_str(speaker_name);
+            std::string speaker_name_str(recv_speaker_name);
             std::string speaker_nnet3_rxfilename = speaker_nnet3_dir + "/" + speaker_name_str + ".mdl";
 
             KALDI_VLOG(1) << "Loading AM...";
